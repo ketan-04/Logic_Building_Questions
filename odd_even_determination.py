5cdb1f359add52435beffe65d81becff12da8799
Ketan Agrawal <ketanagrawal054@gmail.com>
2025-03-24 19:54:08 +0530

    Initial commit: Added logic building questions

---
+++ odd_even_determination.py
@@ -0,0 +1,12 @@
+def odd_even_determination(n):
+    if n==0:
+        print(" enter natural numbers only")
+    elif(n%2==0):
+        print("even")
+    elif(n%2!=0):
+        print('odd')
+    else:
+        print("enter correct number")
+
+# print(odd_even_determination(101))
+odd_even_determination(103)
---
+++ prime_numb_check.py
@@ -0,0 +1,11 @@
+def prime_numb_check(n):
+    if (n ==0):
+        print("Enter Natural Number ONnly !!")
+    elif (n%n == 0 and n%1==0  ):
+        print('PRIME')
+    else:
+        print('enter valid input')
+
+prime_numb_check(9)
+
+## WRROONNGGGGGGGGGGG
